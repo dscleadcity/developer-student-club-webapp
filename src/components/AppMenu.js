e499764e1e9be9b6b70694cac673c0716c9d1dd8
Usheninte <jrandangana@gmail.com>
2018-10-05 20:54:37 +0100

    added AppMenu

---
+++ src/components/AppMenu.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand, NavbarNav, NavbarToggler, Collapse, NavItem, NavLink } from 'mdbreact';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+class AppMenu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapse: false,
+      isWideEnough: false,
+    };
+  this.onClick = this.onClick.bind(this);
+  }
+
+  onClick(){
+    this.setState({
+      collapse: !this.state.collapse,
+    });
+  }
+  render() {
+    return (
+      <Router>
+        <Navbar color="green" dark expand="md" scrolling>
+          <NavbarBrand href="/">
+              <strong>DSC Lead City University</strong>
+          </NavbarBrand>
+          { !this.state.isWideEnough && <NavbarToggler onClick = { this.onClick } />}
+          <Collapse isOpen = { this.state.collapse } navbar>
+            <NavbarNav right>
+              <NavItem>
+                <NavLink to="#">Blog</NavLink>
+              </NavItem>
+              <NavItem active>
+                <NavLink to="#">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to="#">Workshops</NavLink>
+              </NavItem>
+            </NavbarNav>
+          </Collapse>
+        </Navbar>
+      </Router>
+    );
+  }
+}
+
+export default AppMenu;
