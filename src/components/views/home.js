5e421e55264cf1b386d9d76edd253dc9c7362a5d
Usheninte <jrandangana@gmail.com>
2018-10-07 16:45:52 +0100

    built Workshop

--- src/components/views/home.js
+++ src/components/views/home.js
@@ -13,10 +13,16 @@ export default class Home extends Component {
       <div>
         <img style={roundImg} src="https://res.cloudinary.com/poetrique/image/upload/v1538920604/developer-student-club/dsc.png" alt="Developer Student Club" title="Developer Student Club" />
         <h3 id="title" class="white-text">WHAT ARE DEVELOPER STUDENT CLUBS?</h3>
-        <div id="content">
-          <p className="light-green-text">DSC is a Google Developers program for university students, designed to help them build their mobile and web development skills and knowledge. It is open to any student, ranging from novice developers who are just starting, to advanced developers who want to further their skills. It is intended to be a space for students to learn and collaborate as they solve mobile and web development problems.</p>
-          <p className="light-green-text">This is part of Google’s commitment to provide mobile developer training to 100,000 Africans to develop world-class apps in the next 5 years. The Developer Student Clubs (DSC) is a grassroots channel through which we provide development skills for students, towards employability. The program invites students enrolled in African universities to act as “agent of change” on their campus(es).</p>
-          <p className="light-green-text">DSCs follow a model of peer-to-peer learning. We select student Leads who train other students, help them apply the skills by developing solutions for local businesses, and host showcases for students to present their prototypes/solutions. This not only upskills current developers, but also inspires new developers thus realizing rapid scale.</p>
+        <div id="content" class="container">
+          <div class="row">
+            <div class="col"></div>
+            <div class="col-10 col-sm-6">
+              <p className="light-green-text">DSC is a Google Developers program for university students, designed to help them build their mobile and web development skills and knowledge. It is open to any student, ranging from novice developers who are just starting, to advanced developers who want to further their skills. It is intended to be a space for students to learn and collaborate as they solve mobile and web development problems.</p>
+              <p className="light-green-text">This is part of Google’s commitment to provide mobile developer training to 100,000 Africans to develop world-class apps in the next 5 years. The Developer Student Clubs (DSC) is a grassroots channel through which we provide development skills for students, towards employability. The program invites students enrolled in African universities to act as “agent of change” on their campus(es).</p>
+              <p className="light-green-text">DSCs follow a model of peer-to-peer learning. We select student Leads who train other students, help them apply the skills by developing solutions for local businesses, and host showcases for students to present their prototypes/solutions. This not only upskills current developers, but also inspires new developers thus realizing rapid scale.</p>
+            </div>
+            <div class="col"></div>
+          </div>
         </div>
       </div>
     );
--- src/components/views/workshops.js
+++ src/components/views/workshops.js
@@ -1,12 +1,170 @@
 import React, { Component } from 'react';
+import { Card, CardBody, CardImage, CardTitle, CardText } from 'mdbreact';
 
 import '../App.css';
 
-export default class Workshops extends Component {
+export default class CardExample extends Component {
   render() {
-    return(
+    return (
       <div>
-        <h1 id="title" class="white-text">Workshops!</h1>
+        <div class="container">
+
+          <h3 id="title" class="white-text">WORKSHOPS 2018/2019</h3> 
+          
+          <div class="row content">
+            <div class="col"></div>
+            <div class="col-10 col-sm-8">
+              <Card>
+                <CardImage className="img-fluid" src="https://res.cloudinary.com/poetrique/image/upload/v1538925366/dsc-workshops/dscleadcity-WORKSHOP_1.png" waves />
+                <CardBody>
+                  <CardTitle>Introduction to Web Development & Github</CardTitle>
+                  <CardText>
+                    <p>Venue: ROC World Entrepreneurial Development Centre, Vocational Unit 1 Building</p>
+                    <br />
+                    <p><b>Lead City University, Ibadan - NG</b></p>
+                    </CardText>
+                </CardBody>
+              </Card>
+            </div>
+            <div class="col"></div>
+          </div>
+
+          <div class="row content">
+            <div class="col"></div>
+            <div class="col-10 col-sm-8">
+              <Card>
+                <CardBody>
+                  <CardTitle>Build a Node.js web app using Google Cloud</CardTitle>
+                  <CardText>
+                    <p>Venue: ROC World Entrepreneurial Development Centre, Vocational Unit 1 Building</p>
+                    <br />
+                    <p><b>Lead City University, Ibadan - NG</b></p>
+                    </CardText>
+                </CardBody>
+              </Card>
+            </div>
+            <div class="col"></div>
+          </div>
+          <div class="row content">
+            <div class="col"></div>
+            <div class="col-10 col-sm-8">
+              <Card>
+                <CardBody>
+                  <CardTitle>Cloud Firestore and Web</CardTitle>
+                  <CardText>
+                    <p>Venue: ROC World Entrepreneurial Development Centre, Vocational Unit 1 Building</p>
+                    <br />
+                    <p><b>Lead City University, Ibadan - NG</b></p>
+                    </CardText>
+                </CardBody>
+              </Card>
+            </div>
+            <div class="col"></div>
+          </div>
+
+          <div class="row content">
+            <div class="col"></div>
+            <div class="col-10 col-sm-8">
+              <Card>
+                <CardBody>
+                  <CardTitle>App Monetization</CardTitle>
+                  <CardText>
+                    <p>Venue: ROC World Entrepreneurial Development Centre, Vocational Unit 1 Building</p>
+                    <br />
+                    <p><b>Lead City University, Ibadan - NG</b></p>
+                    </CardText>
+                </CardBody>
+              </Card>
+            </div>
+            <div class="col"></div>
+          </div>
+
+          <div class="row content">
+            <div class="col"></div>
+            <div class="col-10 col-sm-8">
+              <Card>
+                <CardBody>
+                  <CardTitle>Using Firebase Test Lab to improve Mobile App quality</CardTitle>
+                  <CardText>
+                    <p>Venue: ROC World Entrepreneurial Development Centre, Vocational Unit 1 Building</p>
+                    <br />
+                    <p><b>Lead City University, Ibadan - NG</b></p>
+                    </CardText>
+                </CardBody>
+              </Card>
+            </div>
+            <div class="col"></div>
+          </div>
+
+          <div class="row content">
+            <div class="col"></div>
+            <div class="col-10 col-sm-8">
+              <Card>
+                <CardBody>
+                  <CardTitle>Using Google Cloud Test Lab to improve Mobile App quality</CardTitle>
+                  <CardText>
+                    <p>Venue: ROC World Entrepreneurial Development Centre, Vocational Unit 1 Building</p>
+                    <br />
+                    <p><b>Lead City University, Ibadan - NG</b></p>
+                    </CardText>
+                </CardBody>
+              </Card>
+            </div>
+            <div class="col"></div>
+          </div>
+
+          <div class="row content">
+            <div class="col"></div>
+            <div class="col-10 col-sm-8">
+              <Card>
+                <CardBody>
+                  <CardTitle>Intro to Android development with Kotlin</CardTitle>
+                  <CardText>
+                    <p>Venue: ROC World Entrepreneurial Development Centre, Vocational Unit 1 Building</p>
+                    <br />
+                    <p><b>Lead City University, Ibadan - NG</b></p>
+                    </CardText>
+                </CardBody>
+              </Card>
+            </div>
+            <div class="col"></div>
+          </div>
+
+          <div class="row content">
+            <div class="col"></div>
+            <div class="col-10 col-sm-8">
+              <Card>
+                <CardBody>
+                  <CardTitle>Build Your First Android App in Kotlin</CardTitle>
+                  <CardText>
+                    <p>Venue: ROC World Entrepreneurial Development Centre, Vocational Unit 1 Building</p>
+                    <br />
+                    <p><b>Lead City University, Ibadan - NG</b></p>
+                    </CardText>
+                </CardBody>
+              </Card>
+            </div>
+            <div class="col"></div>
+          </div>
+
+          <div class="row content">
+            <div class="col"></div>
+            <div class="col-10 col-sm-8">
+              <Card>
+                <CardBody>
+                  <CardTitle>Get your Startup started</CardTitle>
+                  <CardText>
+                    <p>Venue: ROC World Entrepreneurial Development Centre, Vocational Unit 1 Building</p>
+                    <br />
+                    <p><b>Lead City University, Ibadan - NG</b></p>
+                    </CardText>
+                </CardBody>
+              </Card>
+            </div>
+            <div class="col"></div>
+          </div>
+
+        </div>
       </div>
     );
   }
