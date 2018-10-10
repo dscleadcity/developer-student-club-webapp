0dfb942dca36ef9eb670fe4193e4a375190fcc6d
Usheninte <jrandangana@gmail.com>
2018-10-10 12:07:20 +0100

    completed Social component

--- src/components/Social.js
+++ src/components/Social.js
@@ -12,20 +12,22 @@ export default class Social extends Component {
     return (
       <div>
         
-        <h3 style={powerStyle} class ="content">Like & Follow Us On</h3>
+        <h4 style={powerStyle} class="white-text content">Follow Us For Tech Updates</h4>
 
         <div class="container">
           <div class="row">
             <div class="col"></div>
             <div class="col-12 col-sm-6">
-              <div class="col"></div>
-              <div class="col-4">
-                <a href="https://instagram.com/dscleadcity"><i class="fab fa-instagram fa-3x"></i></a>
+              <div class="row">
+                <div class="col"></div>
+                <div class="col-4">
+                  <a href="https://instagram.com/dscleadcity" target="_blank" rel="noopener noreferrer" className="light-green-text"><i class="fab fa-instagram fa-3x"></i></a>
+                </div>
+                <div class="col-4">
+                  <a href="https://twitter.com/dscleadcity" target="_blank" rel="noopener noreferrer" className="light-green-text"><i class="fab fa-twitter fa-3x"></i></a>
+                </div>
+                <div class="col"></div>
               </div>
-              <div class="col-4">
-                <a href="https://twitter.com/dscleadcity"><i class="fab fa-twitter fa-3x"></i></a>
-              </div>
-              <div class="col"></div>
             </div>
             <div class="col"></div>
           </div>
