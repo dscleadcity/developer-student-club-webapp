088b0993e6a939e489146a7a6196fb2e29de94b7
Usheninte <jrandangana@gmail.com>
2018-10-25 05:14:52 +0100

    added Gallery structure

--- src/components/views/gallery.js
+++ src/components/views/gallery.js
@@ -7,6 +7,85 @@ export default class Gallery extends Component {
     return(
       <div>
         <h3 class="white-text content">PICTURES OF THE COMMUNITY</h3>
+        <div class="container">
+          <div class="row">
+            <div class="col-12 col-sm-4">
+              <img src="" alt="" title="DSC Lead City Community" />
+            </div>
+            <div class="col-12 col-sm-4">
+              <img src="" alt="" title="DSC Lead City Community" />
+            </div>
+            <div class="col-12 col-sm-4">
+              <img src="" alt="" title="DSC Lead City Community" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12 col-sm-4">
+              <img src="" alt="" title="DSC Lead City Community" />
+            </div>
+            <div class="col-12 col-sm-4">
+              <img src="" alt="" title="DSC Lead City Community" />
+            </div>
+            <div class="col-12 col-sm-4">
+              <img src="" alt="" title="DSC Lead City Community" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12 col-sm-4">
+              <img src="" alt="" title="DSC Lead City Community" />
+            </div>
+            <div class="col-12 col-sm-4">
+              <img src="" alt="" title="DSC Lead City Community" />
+            </div>
+            <div class="col-12 col-sm-4">
+              <img src="" alt="" title="DSC Lead City Community" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12 col-sm-4">
+              <img src="" alt="" title="DSC Lead City Community" />
+            </div>
+            <div class="col-12 col-sm-4">
+              <img src="" alt="" title="DSC Lead City Community" />
+            </div>
+            <div class="col-12 col-sm-4">
+              <img src="" alt="" title="DSC Lead City Community" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12 col-sm-4">
+              <img src="" alt="" title="DSC Lead City Community" />
+            </div>
+            <div class="col-12 col-sm-4">
+              <img src="" alt="" title="DSC Lead City Community" />
+            </div>
+            <div class="col-12 col-sm-4">
+              <img src="" alt="" title="DSC Lead City Community" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12 col-sm-4">
+              <img src="" alt="" title="DSC Lead City Community" />
+            </div>
+            <div class="col-12 col-sm-4">
+              <img src="" alt="" title="DSC Lead City Community" />
+            </div>
+            <div class="col-12 col-sm-4">
+              <img src="" alt="" title="DSC Lead City Community" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12 col-sm-4">
+              <img src="" alt="" title="DSC Lead City Community" />
+            </div>
+            <div class="col-12 col-sm-4">
+              <img src="" alt="" title="DSC Lead City Community" />
+            </div>
+            <div class="col-12 col-sm-4">
+              <img src="" alt="" title="DSC Lead City Community" />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
